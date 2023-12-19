ef258d7fe333209a6c996631c596a39997216707
ying_123 <zhangying@aten-optics.com>
2023-12-19 10:53:28 +0800

    update api

--- src/adaptors/gequbao.ts
+++ src/adaptors/gequbao.ts
@@ -41,7 +41,7 @@ export async function getMusicSearch(s: string): Promise<SearchMusic[] | null> {
 
 function parsePosterUrl(html: string) {
     const matchBlock = html.match(
-        /cover:\s'https?:\/\/[^']+'/
+        /mp3_cover\s=\s'https?:\/\/[^']+'/
     )
     return matchBlock ? matchBlock[0].match(/https?:\/\/[^']+/)![0] : null
 }
