b38c4afdf801d6926c0b70b6ad5cbe60cf744ebd
icefee <icefee@outlook.com>
2024-09-27 09:30:16 +0800

    update api endpoint

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,4 +1,4 @@
 export abstract class Api {
-    public static site = 'http://cik.netlify.app'
+    public static site = 'https://code-space-mauve.vercel.app'
     public static proxy = 'https://astro-app-7d4.pages.dev'
 }
