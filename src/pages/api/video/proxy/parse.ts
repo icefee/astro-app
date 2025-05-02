5dcd1835843ea95b0154479f5f95bcb443a5859f
icefee <icefee@outlook.com>
2025-05-03 02:21:03 +0800

    add parse api

---
+++ src/pages/api/video/proxy/parse.ts
@@ -0,0 +1,44 @@
+import type { APIRoute } from 'astro'
+import { getJson } from '@adaptors/common'
+import { httpHeaders } from '@util/common'
+import { withHost } from './'
+import { parseProxyVideoData } from '@util/crypto'
+
+export const GET: APIRoute = async ({ url }) => {
+    const params = url.searchParams
+    const headers = {
+        ...httpHeaders.json,
+        ...httpHeaders.cors
+    }
+    try {
+        const host = await withHost(params)
+        const query = {
+            id: +params.get('id')!
+        }
+        const data = await getJson<ProxyVideo.ApiJson>(
+            `https://${host}/api/p1/x1_q2_aaaa`,
+            {
+                method: 'POST',
+                headers: httpHeaders.json,
+                body: JSON.stringify(query)
+            }
+        )
+        return Response.json({
+            code: 0,
+            data: parseProxyVideoData<ProxyVideo.SearchVideo>(data),
+            msg: '成功'
+        }, {
+            headers
+        })
+    }
+    catch (err) {
+        return Response.json({
+            code: -1,
+            data: null,
+            msg: String(err)
+        }, {
+            status: 500,
+            headers
+        })
+    }
+}
