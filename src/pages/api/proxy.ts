d0ce87e717c5839975b5e13fb68e1fddd445d2bd
icefee <icefee@outlook.com>
2025-06-22 00:00:15 +0800

    update proxy api

--- src/pages/api/proxy.ts
+++ src/pages/api/proxy.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from 'astro'
 import { httpHeaders, unsafe_fetch } from '@util/common'
 import { userAgent } from '@util/env'
+import { hrtime } from 'process';
 
 const inheritedHeaders: Array<{
     key: string;
@@ -34,12 +35,12 @@ const inheritedHeaders: Array<{
 
 export const GET: APIRoute = async ({ url, request }) => {
     const params = url.searchParams
-    const targetUrl = params.get('url'), cors = params.get('cors') === '1'
-    if (targetUrl) {
+    const target = params.get('url'), cors = params.get('cors') === '1'
+    if (target) {
         const requestHeaders = new Headers(request.headers)
         requestHeaders.delete('host')
         requestHeaders.set('user-agent', userAgent)
-        const { body, status, headers: originHeaders } = await unsafe_fetch(targetUrl, {
+        const { body, status, headers: originHeaders } = await unsafe_fetch(target, {
             headers: requestHeaders
         })
         const headers = new Headers(cors ? httpHeaders.cors : undefined)
@@ -53,6 +54,42 @@ export const GET: APIRoute = async ({ url, request }) => {
             status,
             headers
         })
+    }
+    return new Response('invalid url', {
+        headers: httpHeaders.html
+    })
+}
+
+export const POST: APIRoute = async ({ url, request }) => {
+    const params = url.searchParams
+    const target = params.get('url')
+    if (target) {
+        const headers = new Headers(request.headers)
+        headers.delete('host')
+        headers.set('user-agent', userAgent)
+        const response = await unsafe_fetch(target, {
+            method: 'POST',
+            headers,
+            body: await request.text()
+        })
+        const responseHeaders = new Headers(response.headers)
+        responseHeaders.delete('content-encoding')
+        try {
+            const data = await response.json()
+            return new Response(JSON.stringify(data), {
+                status: response.status,
+                headers: {
+                    ...responseHeaders,
+                    ...httpHeaders.json
+                }
+            })
+        }
+        catch (err) {
+            return new Response(await response.blob(), {
+                status: response.status,
+                headers: responseHeaders
+            })
+        }
     }
     return new Response('invalid url', {
         headers: httpHeaders.html
