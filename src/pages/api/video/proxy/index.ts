5be4cad4f6c047bbce32868edb9525814a47f2b2
icefee <icefee@outlook.com>
2025-08-21 20:52:59 +0800

    update proxy api host match rule

--- src/pages/api/video/proxy/index.ts
+++ src/pages/api/video/proxy/index.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from 'astro'
 import { getText } from '@adaptors/common'
 import { httpHeaders } from '@util/common'
 import { isDev } from '@util/env'
-// import { utf8Tobase64 } from '@util/base64'
+import { utf82utf16 } from '@util/parser'
 import { Api } from '@util/config'
 
 const checkUrl = 'https://8x8x.com'
@@ -15,29 +15,49 @@ export const getHtml = (url: string) => getText(url, {
     headers: httpHeaders.client
 })
 
-async function getMatch(url: string, reg: RegExp) {
+async function getMatch(url: string, reg: RegExp): Promise<Record<'source' | 'result', string | null>> {
     try {
-        const html = await getHtml(url)
-        const matches = html.match(reg)
+        const source = await getHtml(url)
+        const matches = source.match(reg)
         if (matches) {
-            return matches[0]
+            return {
+                source,
+                result: matches[0]
+            }
         }
-        throw new Error('Get match error')
+        return {
+            source,
+            result: null
+        }
     }
     catch (err) {
-        return null
+        return {
+            source: null,
+            result: null
+        }
     }
 }
 
 async function checkHost() {
     console.log('Start check host...')
     const urlMatchReg = /([a-z\d]{3,}\.)+[a-z]{2,4}/g
-    const matchBlock = await getMatch(
+    const blockMatchReg = new RegExp(`<br class="showBr"><a href="https?://${urlMatchReg.source}"`)
+    let { source, result } = await getMatch(
         isDev ? `${Api.proxy}/api/proxy?url=${checkUrl}` : checkUrl,
-        new RegExp(`最新地址一：<br class="showBr"><a href="https?://${urlMatchReg.source}"`)
+        blockMatchReg
     )
-    const host = matchBlock?.match(urlMatchReg)?.[0]
-    return host ? host : null
+    if (!result) {
+        if (!source) {
+            return null
+        }
+        const base64Text = source.match(
+            new RegExp(`base64\\s=\\s"[\\w\\/\\+]+\={0,2}"`)
+        )![0]
+        const base64 = base64Text.slice(10, base64Text.length - 1)
+        source = utf82utf16(atob(base64))
+        result = source.match(blockMatchReg)![0]
+    }
+    return result.match(urlMatchReg)?.[0]
 }
 
 export async function withHost(params?: URLSearchParams) {
