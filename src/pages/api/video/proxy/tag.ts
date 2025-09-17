53837fba7324e608bc755860ba457dfb8dca3897
icefee <icefee@outlook.com>
2025-09-17 09:30:47 +0800

    update proxy api

--- src/pages/api/video/proxy/tag.ts
+++ src/pages/api/video/proxy/tag.ts
@@ -2,10 +2,11 @@ import type { APIRoute } from 'astro'
 import { host, proxyResponse } from '.'
 import { httpHeaders } from '@util/common'
 
-export const GET: APIRoute = async ({ url, request }) => {
+export const POST: APIRoute = async ({ url, request }) => {
+    const category = url.searchParams.get('c')
+    const api = category ? 'f1l2/l1_aaaa' : 'b1q2/l1_cccc'
     const body = await request.json()
-    const target = `https://${host}/api/${url.searchParams.get('c') ? 'f1l2/l1_aaaa' : 'b1q2/l1_cccc'}`
-    return proxyResponse(target, {
+    return proxyResponse(`https://${host}/api/${api}`, {
         method: 'post',
         body: JSON.stringify(body),
         headers: {
